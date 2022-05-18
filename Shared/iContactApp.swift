468e1d5ac1ffae617095f948c1d38e2327f60731
Lucas Chae <hi@lucaschae.com>
2022-05-18 15:07:44 +0900

    Initial Commit

---
+++ Shared/iContactApp.swift
@@ -0,0 +1,17 @@
+//
+//  iContactApp.swift
+//  Shared
+//
+//  Created by Lucas Chae on 5/18/22.
+//
+
+import SwiftUI
+
+@main
+struct iContactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
